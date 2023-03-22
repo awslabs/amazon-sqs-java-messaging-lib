318e8768d61b93a6a33d2f9e6d5f2d30941b4fc9
Vedran Pavic <vedran@vedranpavic.com>
2023-03-22 22:55:07 +0100

    Remove Lombok

--- src/main/java/com/amazon/sqs/javamessaging/acknowledge/SQSMessageIdentifier.java
+++ src/main/java/com/amazon/sqs/javamessaging/acknowledge/SQSMessageIdentifier.java
@@ -16,31 +16,19 @@ package com.amazon.sqs.javamessaging.acknowledge;
 
 import com.amazon.sqs.javamessaging.message.SQSMessage;
 import jakarta.jms.JMSException;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
 
 /**
  * Identifies an SQS message, when (negative)acknowledging the message
  */
-@ToString(onlyExplicitlyIncluded = true)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Getter
 public class SQSMessageIdentifier {
 
     // The queueUrl where the message was sent or received from
-    @ToString.Include
-    @EqualsAndHashCode.Include
     private final String queueUrl;
 
     // The receipt handle returned after the delivery of the message from SQS
-    @ToString.Include
-    @EqualsAndHashCode.Include
     private final String receiptHandle;
 
     // The SQS message id assigned on send.
-    @ToString.Include
-    @EqualsAndHashCode.Include
     private final String sqsMessageId;
     
     // The group id to which the message belongs
@@ -62,5 +50,82 @@ public class SQSMessageIdentifier {
     
     public static SQSMessageIdentifier fromSQSMessage(SQSMessage sqsMessage) throws JMSException {
         return new SQSMessageIdentifier(sqsMessage.getQueueUrl(), sqsMessage.getReceiptHandle(), sqsMessage.getSQSMessageId(), sqsMessage.getSQSMessageGroupId());
+    }
+
+    /**
+     * Returns the queueUrl where the message was sent or received from.
+     *
+     * @return queueUrl
+     */
+    public String getQueueUrl() {
+        return this.queueUrl;
+    }
+
+    /**
+     * Returns the receipt handle returned after the delivery of the message
+     * from SQS.
+     *
+     * @return receiptHandle
+     */
+    public String getReceiptHandle() {
+        return this.receiptHandle;
+    }
+
+    /**
+     * Returns the SQS message id assigned on send.
+     *
+     * @return sqsMessageId
+     */
+    public String getSQSMessageID() {
+        return this.sqsMessageId;
+    }
+
+    /**
+     * Returns the group id to which the message belongs. Non-null only for messages received from FIFO queues.
+     *
+     * @return groupId
+     */
+    public String getGroupId() {
+        return this.groupId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((queueUrl == null) ? 0 : queueUrl.hashCode());
+        result = prime * result + ((receiptHandle == null) ? 0 : receiptHandle.hashCode());
+        result = prime * result + ((sqsMessageId == null) ? 0 : sqsMessageId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SQSMessageIdentifier other = (SQSMessageIdentifier) obj;
+        if (queueUrl == null) {
+            if (other.queueUrl != null)
+                return false;
+        } else if (!queueUrl.equals(other.queueUrl))
+            return false;
+        if (receiptHandle == null) {
+            if (other.receiptHandle != null)
+                return false;
+        } else if (!receiptHandle.equals(other.receiptHandle))
+            return false;
+        if (sqsMessageId == null) {
+            return other.sqsMessageId == null;
+        } else return sqsMessageId.equals(other.sqsMessageId);
+    }
+
+    @Override
+    public String toString() {
+        return "SQSMessageIdentifier [queueUrl=" + queueUrl + ", receiptHandle=" + receiptHandle +
+               ", sqsMessageId=" + sqsMessageId + "]";
     }
 }
